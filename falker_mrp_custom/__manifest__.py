ec072bd8693cb11f46c37cf7ae5c6a52a3aa065f
Kaynnan Lemes <kaynnanx1@gmail.com>
2025-06-12 11:20:59 -0300

    [ADD] falker_mrp_custom: add new module

---
+++ falker_mrp_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Falker Mrp Custom",
+    "summary": """
+        Falker MRP Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/falker-addons",
+    "depends": ["mrp"],
+    "data": [
+        "security/falker_security.xml",
+        "security/ir.model.access.csv",
+        "views/product_product.xml",
+        "views/product_template.xml",
+    ],
+}
