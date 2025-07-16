9cacf9a538c7fdaecb49950ccd9a7293cf31b158
edinhokrs <edinhokrause@hotmail.com>
2025-07-16 15:48:29 -0300

    [IMP] falker_sale_price_control: Add user group permissions for price editing

--- falker_sale_price_control/models/sale_order_line.py
+++ falker_sale_price_control/models/sale_order_line.py
@@ -18,42 +18,43 @@ class SaleOrderLine(models.Model):
             else:
                 line.fiscal_price = line.price_unit
 
-    @api.model
-    def create(self, values):
-        return super().create(values)
-
     def write(self, vals):
-        # Checks if the order has a different pricelist from the one in the context
-        if "pricelist_id" not in vals and any(
-            line.order_id.pricelist_id.id
-            != self.env.context.get("default_pricelist_id")
-            for line in self
-            if line.order_id
-        ):
-            vals["price_unit"] = self._get_price_from_pricelist()
+        if self.env.user.has_group("falker_sale_price_control.group_price_editor"):
+            # Users with permission - price synchronization
+            if "price_unit" in vals:
+                vals["fiscal_price"] = vals["price_unit"]
+            elif "fiscal_price" in vals:
+                vals["price_unit"] = vals["fiscal_price"]
+        else:
+            # Users without permission - apply rules
+            if "pricelist_id" not in vals and any(
+                line.order_id.pricelist_id.id
+                != self.env.context.get("default_pricelist_id")
+                for line in self
+                if line.order_id
+            ):
+                vals["price_unit"] = self._get_price_from_pricelist()
 
-        # Att when change the product
-        if "product_id" in vals:
-            for line in self:
-                order = line.order_id
-                product = self.env["product.product"].browse(vals["product_id"])
-                quantity = vals.get("product_uom_qty", line.product_uom_qty)
+            if "product_id" in vals:
+                for line in self:
+                    order = line.order_id
+                    product = self.env["product.product"].browse(vals["product_id"])
+                    quantity = vals.get("product_uom_qty", line.product_uom_qty)
 
-                if order.pricelist_id:
-                    price = order.pricelist_id.with_context(
-                        uom=product.uom_id.id, date=order.date_order
-                    ).get_product_price(product, quantity, order.partner_id)
+                    if order.pricelist_id:
+                        price = order.pricelist_id.with_context(
+                            uom=product.uom_id.id, date=order.date_order
+                        ).get_product_price(product, quantity, order.partner_id)
 
-                    vals["price_unit"] = price
-                    vals["fiscal_price"] = price
+                        vals["price_unit"] = price
+                        vals["fiscal_price"] = price
 
         return super().write(vals)
 
     def _get_price_from_pricelist(self):
-        """Obtém o preço atualizado da lista de preços com tratamento para linhas vazias"""
+        """Get price from price list"""
         self.ensure_one()
 
-        # If it's a line without product, return 0
         if not self.product_id or self.display_type == "line_note":
             return 0.0
 
@@ -63,9 +64,16 @@ class SaleOrderLine(models.Model):
             self.product_id, self.product_uom_qty, self.order_id.partner_id
         )
 
-    @api.onchange("product_id")
-    def _onchange_product_id_custom_price(self):
-        if not self.product_id:
-            return
-        self.product_id_change()
+    @api.onchange("product_id", "product_uom_qty")
+    def _onchange_product_quantity(self):
+        """Att price when change product or quantity"""
+        if not self.env.user.has_group("falker_sale_price_control.group_price_editor"):
+            if self.product_id:
+                price = self._get_price_from_pricelist()
+                self.price_unit = price
+                self.fiscal_price = price
+
+        # Onchange original from Odoo
+        if hasattr(super(), "product_id_change"):
+            super().product_id_change()
         self._compute_tax_id()
