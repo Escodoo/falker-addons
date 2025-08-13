1a34bcba80fbdb5e45f507648ffb7c6b28146c4e
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-08-13 16:32:38 -0300

    [IMP] mautic_connector: lead

--- mautic_connector/__manifest__.py
+++ mautic_connector/__manifest__.py
@@ -6,12 +6,19 @@
     "author": "Escodoo, Odoo Community Association (OCA)",
     "website": "https://github.com/Escodoo/falker-addons",
     "license": "AGPL-3",
-    "depends": ["account", "sale", "sale_management", "account_asset_management"],
+    "depends": [
+        "account",
+        "sale",
+        "sale_management",
+        "account_asset_management",
+        "crm",
+    ],
     "data": [
         # Security
         "security/ir.model.access.csv",
         # Data
         "data/mautic_cron.xml",
+        "data/crm_tag_falker.xml",
         # Views
         "views/res_company.xml",
         "views/res_partner.xml",
