b3dea444747776c0f7efa093366e4672ad666b75
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-08-13 16:32:38 -0300

    [IMP] mautic_connector: lead

--- mautic_connector/models/mautic_sync_log.py
+++ mautic_connector/models/mautic_sync_log.py
@@ -1,6 +1,6 @@
 # Copyright 2025 - TODAY, Cristiano Mafra Junior <cristiano.mafra@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MauticSyncLog(models.Model):
@@ -8,10 +8,13 @@ class MauticSyncLog(models.Model):
     _description = "Synchronization Log with Mautic"
     _order = "execution_time desc"
 
+    name = fields.Char()
+
     sync_type = fields.Selection(
         [
             ("contact", "Contact"),
             ("company", "Company"),
+            ("lead", "Lead"),
         ],
         required=True,
     )
@@ -31,3 +34,19 @@ class MauticSyncLog(models.Model):
         ],
         default="done",
     )
+
+    @api.depends("sync_type", "execution_time", "success_count", "error_count")
+    def name_get(self):
+        result = []
+        for record in self:
+            exec_date = (
+                record.execution_time.strftime("%Y-%m-%d %H:%M")
+                if record.execution_time
+                else ""
+            )
+            sync_label = dict(self._fields["sync_type"].selection).get(
+                record.sync_type, ""
+            )
+            display_name = f"[{exec_date}] {sync_label}"
+            result.append((record.id, display_name))
+        return result
